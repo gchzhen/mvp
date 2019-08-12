09fc6405e0b7c8d6bfe8b963ac4cdb56fe2d3cbd
Galina Chzhen <g.chzhen@gmail.com>
2019-08-12 15:23:41 -0700

    Add test endpoint and render on the screen

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 export default class Main extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      json: null
+    };
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/test')
+    .then((response) => response.json())
+    .then((json) => this.setState({ json: json }))
+    .catch((err) => console.log(err))
   }
 
   render (){
     return (
-      <h1>Hello from react!</h1>
+      <h1>{this.state.json}</h1>
     )
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const bodyParser = require('body-parser');
 
@@ -7,6 +6,10 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// app.use(cors());
+
+app.get('/test', (req, res) => res.json('Hello from server, endpoint /test'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
