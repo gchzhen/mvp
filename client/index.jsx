446aa6e20cab0b6d46c46bb3266f16f09132cecf
Galina Chzhen <g.chzhen@gmail.com>
2019-08-12 15:08:20 -0700

    Connect client to server/express serves static files

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,5 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './src/Main.jsx';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+ReactDOM.render( <Main/>, document.getElementById('root') );
---
+++ client/src/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render (){
+    return (
+      <h1>Hello from react!</h1>
+    )
+  }
+};
